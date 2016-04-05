d33d68f1fae3137c348cb19b870735dc9691397a
sago35 <sago35@gmail.com>
2016-04-06 00:50:15 +0900

    client、roomを追加

---
+++ client.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	"log"
+)
+
+type client struct {
+	socket *websocket.Conn
+	send   chan []byte
+	room   *room
+}
+
+func (c *client) read() {
+	for {
+		if _, msg, err := c.socket.ReadMessage(); err == nil {
+			c.room.forward <- msg
+		} else {
+			break
+		}
+	}
+	log.Println("read end")
+	c.socket.Close()
+}
+
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+			break
+		}
+	}
+	log.Println("write end")
+	c.socket.Close()
+}
